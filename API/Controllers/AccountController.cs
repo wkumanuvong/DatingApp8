d8c51639d4062d0d3ce682aeca6a4c7ffcc91407
wkumanuvong <waleerat@msn.com>
2024-05-30 20:50:12 +0700

    End of section 4

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,75 @@
+﻿﻿using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController : BaseApiController
+{
+    private readonly DataContext _context;
+    private readonly ITokenservice _tokenService;
+
+    public AccountController(DataContext context, ITokenservice tokenService)
+    {
+        _context = context;
+        _tokenService = tokenService;
+    }
+
+    [HttpPost("register")] // POST: api/account/register?username=dave&password=pwd
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+    {
+        if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+        // derived from HashAlgorithm, which implement IDisposable interface, which need to implement a Dispose method
+        // with using keyword => after finish with the class then that Dispose method will be called and the class is removed from memory
+        using var hmac = new HMACSHA512();
+        var user = new AppUser
+        {
+            UserName = registerDto.Username.ToLower(),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        return new UserDto
+        {
+            Username = user.UserName,
+            Token = _tokenService.CreateToken(user)
+        };
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+    {
+        var user = await _context.Users.SingleOrDefaultAsync(x => 
+            x.UserName == loginDto.Username);
+
+        if (user == null) return Unauthorized("invalid username");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (int i=0; i < computeHash.Length; i++)
+        {
+            if (computeHash[i] != user.PasswordHash[i]) return Unauthorized("invalid password");            
+        }
+
+        return new UserDto
+        {
+            Username = user.UserName,
+            Token = _tokenService.CreateToken(user)
+        };
+    }
+
+    private async Task<bool> UserExists(string username)
+    {
+        return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+    }
+}
---
+++ API/Interfaces/ITokenService.cs
@@ -0,0 +1,8 @@
+﻿using API.Entities;
+
+namespace API.Interfaces;
+
+public interface ITokenservice
+{
+    string CreateToken(AppUser user);
+}
