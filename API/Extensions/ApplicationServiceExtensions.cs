a31a43ab62963c56152b21d27a812d0332b92870
wkumanuvong <waleerat@msn.com>
2024-06-04 16:56:51 +0700

    End of section 8

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -10,14 +10,16 @@ public static class ApplicationServiceExtensions
     public static IServiceCollection AddApplicationServices(this IServiceCollection services,
         IConfiguration config)
     {
-        services.AddDbContext<DataContext>(opt => 
+        services.AddDbContext<DataContext>(opt =>
         {
             opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
         });
 
         services.AddCors();
         services.AddScoped<ITokenservice, TokenService>();
-        
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
         return services;
     }
 }
