fb8ebd7d751e0a66d76ec6acdfc7f2d9db215746
wkumanuvong <waleerat@msn.com>
2024-06-11 20:07:56 +0700

    End of section 14

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -77,7 +77,7 @@ export class MembersService {
       .find((member: Member) => member.userName === username);
 
     if (member) return of(member);
-    
+
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
@@ -96,6 +96,18 @@ export class MembersService {
 
   deletePhoto(photoId: number) {
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
+  }
+
+  addLike(username: string) {
+    return this.http.post(this.baseUrl + 'likes/' + username, {});
+  }
+
+  getLikes(predicate: string, pageNumber: number, pageSize: number) {
+    let params = this.getPaginationHeaders(pageNumber, pageSize);
+
+    params = params.append('predicate', predicate);
+
+    return this.getPaginatedResult<Member[]>(this.baseUrl + 'likes', params);
   }
 
   private getPaginatedResult<T>(url: string, params: HttpParams) {
